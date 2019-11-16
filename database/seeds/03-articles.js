ece2d5b5f3010cf93f743de1dd67e74e86e9eae1
Michael Harms <michael.harms6010@gmail.com>
2019-11-16 11:36:05 -0600

    sized article dummy url's more realistically

--- database/seeds/03-articles.js
+++ database/seeds/03-articles.js
@@ -2,11 +2,11 @@ exports.seed = function(knex, Promise) {
     return knex('articles').del()
       .then(function () {
         return knex('articles').insert([
-          {url: "url 1", article_label: 'CRISPR babies are totally fine', board_id: 1,},
-          {url: "url 2", article_label: 'Cooking with acids: Ceviche and Beyond', board_id: 2},
-          {url: "url 3", article_label: `Maybe CRISPR babies aren't fine`, board_id: 1},
-          {url: "url 4", article_label: `Bitcoin's fungibility problem: Zcash and Beyond the Infinite`, board_id: 3},
-          {url: "url 5", article_label: `Virality, Humor, and Contrast`, board_id: 4}
+          {url: "https://fakeurl.fakeurl.com/fakearticle.pdf", article_label: 'CRISPR babies are totally fine', board_id: 1,},
+          {url: "https://fakeurl1.fakeurl1.com/fakearticle.pdf", article_label: 'Cooking with acids: Ceviche and Beyond', board_id: 2},
+          {url: "https://fakeurl2.fakeurl2.com/fakearticle.pdf", article_label: `Maybe CRISPR babies aren't fine`, board_id: 1},
+          {url: "https://fakeurl3.fakeurl3.com/fakearticle.pdf", article_label: `Bitcoin's fungibility problem: Zcash and Beyond the Infinite`, board_id: 3},
+          {url: "https://fakeurl4.fakeurl4.com/fakearticle.pdf", article_label: `Virality, Humor, and Contrast`, board_id: 4}
         ]);
       });
   };
